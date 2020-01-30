00a9cf992a9598c70dd0b98bc23f6931cf7f258b
Allan Wanderley <awallan259@gmail.com>
2020-01-29 21:52:24 -0300

    Updates

---
+++ precache-manifest.4e2cb8fc5dc31532533b89352744e412.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd87e2cf8779b6b4c624e7253a58f348",
+    "url": "/simple-list-react/index.html"
+  },
+  {
+    "revision": "b7ef48f243b8c0745c42",
+    "url": "/simple-list-react/static/css/main.b7a9f038.chunk.css"
+  },
+  {
+    "revision": "362575c1c8b463a226ef",
+    "url": "/simple-list-react/static/js/2.e274d660.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/simple-list-react/static/js/2.e274d660.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7ef48f243b8c0745c42",
+    "url": "/simple-list-react/static/js/main.56949bef.chunk.js"
+  },
+  {
+    "revision": "ff6b5c0a3c846c56ab18",
+    "url": "/simple-list-react/static/js/runtime-main.35a4baf2.js"
+  },
+  {
+    "revision": "cfca4129f4539a656da403bb435b9bd9",
+    "url": "/simple-list-react/static/media/1.cfca4129.jpg"
+  },
+  {
+    "revision": "f0a16bc8994052a64c750f87d856efc0",
+    "url": "/simple-list-react/static/media/2.f0a16bc8.jpg"
+  },
+  {
+    "revision": "ebf01c8a31e7c9ee633c15dd08ef9ea4",
+    "url": "/simple-list-react/static/media/3.ebf01c8a.jpg"
+  },
+  {
+    "revision": "24f3c60e5d973d2cca00aaf353a2f398",
+    "url": "/simple-list-react/static/media/4.24f3c60e.jpg"
+  },
+  {
+    "revision": "2d2b2b16c2b80931d9397f5e479e55a6",
+    "url": "/simple-list-react/static/media/5.2d2b2b16.jpg"
+  },
+  {
+    "revision": "8d95bab3d22d2a93c6a305a303b5cc25",
+    "url": "/simple-list-react/static/media/6.8d95bab3.jpg"
+  }
+]);
